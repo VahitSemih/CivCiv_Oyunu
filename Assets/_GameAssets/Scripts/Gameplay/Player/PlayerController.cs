ac6cfea535b1118b09367ee6d7dced8fcf843502
VahitSemih <vahit0344@gmail.com>
2025-07-14 11:43:17 +0300

    v0.12 Health System Added

--- Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/Gameplay/Player/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public event Action OnPlayerJumped;
+
     public event Action<PlayerState> OnPlayerStateChanged;
 
     [Header("References")]
